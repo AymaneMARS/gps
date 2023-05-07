7f8551bcf87a6abc96e501dd724f5662197df9ac
ayman <aymanemars2003@hotmail.com>
2023-05-07 19:46:33 +0100

    ok gps

---
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.gps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap myMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        myMap = googleMap;
+
+        LatLng casablanca = new LatLng(33.48025259367308, -7.6189140722597175);
+        myMap.addMarker(new MarkerOptions().position(casablanca).title("Casablanca"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(casablanca));
+    }
+}
