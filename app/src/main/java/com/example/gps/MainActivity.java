cfa22a5919f249157d188832ca0654e7a42db116
ayman <aymanemars2003@hotmail.com>
2023-05-08 16:52:34 +0100

    ok gps

--- app/src/main/java/com/example/gps/MainActivity.java
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.gps;
-
+import android.Manifest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -24,15 +28,44 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+
     }
+
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
         myMap = googleMap;
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            // Activer la localisation
+            myMap.setMyLocationEnabled(true);
+            myMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                @Override
+                public void onMyLocationChange(Location location) {
+                    // Mettre à jour la caméra de la carte pour suivre la localisation de l'utilisateur
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+                }
+            });
+        }
+
         LatLng casablanca = new LatLng(33.48025259367308, -7.6189140722597175);
-        myMap.addMarker(new MarkerOptions().position(casablanca).title("Casablanca"));
+        BitmapDescriptor icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
+        myMap.addMarker(new MarkerOptions()
+                .position(casablanca)
+                .title("Casablanca")
+                .icon(icon));
+
         myMap.moveCamera(CameraUpdateFactory.newLatLng(casablanca));
+
     }
+
 }
+
